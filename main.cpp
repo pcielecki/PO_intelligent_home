28a2a4d8f155652e7fa0294fd2c104efa066b215
piotr <cielecki.piotr@gmail.com>
2016-04-30 21:38:00 +0200

    added files

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Incl/Models/Dummy_Model.hpp"
+#include "Incl/Adapters/ToDisplayAdapter.hpp"
+#include "Incl/Views/Console.hpp"
+
+using namespace Smart_house;
+
+int main(void)
+{
+	Model::createModel("DummyTemp 001 Temperature //dev//cpp");
+
+
+
+
+	return 0;
+}

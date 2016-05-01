bf60ae87b7fe519499d7fb540222a91bd778cbf7
piotr <cielecki.piotr@gmail.com>
2016-05-01 16:32:14 +0200

    sth went wrong, main.cpp up-to-date now

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
-#include "Incl/Models/Dummy_Model.hpp"
-#include "Incl/Adapters/ToDisplayAdapter.hpp"
-#include "Incl/Views/Console.hpp"
+#include "Incl/Smart_house_iface/Model.hpp"
+#include "Incl/Smart_house_iface/View.hpp"
+#include "Incl/Smart_house_iface/Adapter.hpp"
 
 using namespace Smart_house;
 
 int main(void)
 {
-	Model::createModel("DummyTemp 001 Temperature //dev//cpp");
+	Model* m = 	Model::createModel("DummyTemp 001 Temperature //dev//cpp");
+	View* v = 	View::createView("Console 001");
+	Adapter* a =Adapter::createAdapter("ToDisplay piotr");
 
+	a->pair(m, v);
 
+	a->setParam("unit=K");
+	a->refresh();
 
 
 	return 0;
